841c1384bf967ae47ad7513dc338ae54309ed807
yyyhn1998 <yin.haon@northeastern.edu>
2025-02-01 23:24:06 -0500

    Fixed build errors and updated AssignmentIcon

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+
 import { Form, Button, Container } from "react-bootstrap";
 
 export default function Profile() {
--- src/Kambaz/Courses/Assignments/AssignmentIcon.tsx
+++ src/Kambaz/Courses/Assignments/AssignmentIcon.tsx
@@ -1,7 +1,11 @@
 import { BsFileEarmarkText } from "react-icons/bs";
 
-export default function AssignmentIcon() {
+interface AssignmentIconProps {
+    className?: string;
+}
+
+export default function AssignmentIcon({ className }: AssignmentIconProps) {
     return (
-        <BsFileEarmarkText className="text-success me-2 fs-5" />
+        <BsFileEarmarkText className={`text-success me-2 fs-5 ${className || ""}`} />
     );
 }
