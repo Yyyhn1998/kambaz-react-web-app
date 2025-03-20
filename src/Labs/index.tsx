56f2c4ff4594b09e5627e1e1efb75f1f626b1fe8
yyyhn1998 <yin.haon@northeastern.edu>
2025-03-19 22:59:23 -0400

    render

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -13,23 +13,29 @@ export default function Labs() {
         <Provider store={store}>
         <div id="wd-lab-page">
             <div id="labs-container">
-            <h1>Labs</h1>
-            <h3>Haoning Yin</h3>
-            <h3>CS5610 SEC01</h3>
-            code repository:<br/>
+                <h1>Labs</h1>
+                <h3>Haoning Yin</h3>
+                <h3>CS5610 SEC01</h3>
+                code repository:<br/>
                 <div id="wd-github">
-            <a href="https://github.com/Yyyhn1998/kambaz-react-web-app" id="wd-github">click here</a>
-            to view my GitHub repository
+                    <a href="https://github.com/Yyyhn1998/kambaz-react-web-app" id="wd-github">click
+                        here</a>
+                    to view my GitHub repository
                 </div>
-            <TOC/>
-            <Routes>
-                <Route path="/" element={<Navigate to="Lab1"/>}/>
-                <Route path="Lab1" element={<Lab1/>}/>
-                <Route path="Lab2/*" element={<Lab2/>}/>
-                <Route path="Lab3/*" element={<Lab3 />} />
-                <Route path="Lab4/*" element={<Lab4 />} />
-                <Route path="Lab5/*" element={<Lab5 />} />
-            </Routes>
+                <div id="wd-render">
+                    <a href="https://kambaz-node-server-app-tecy.onrender.com" id="wd-github">click
+                        here</a>
+                    to view my render
+                </div>
+                <TOC/>
+                <Routes>
+                    <Route path="/" element={<Navigate to="Lab1"/>}/>
+                    <Route path="Lab1" element={<Lab1/>}/>
+                    <Route path="Lab2/*" element={<Lab2/>}/>
+                    <Route path="Lab3/*" element={<Lab3/>}/>
+                    <Route path="Lab4/*" element={<Lab4/>}/>
+                    <Route path="Lab5/*" element={<Lab5/>}/>
+                </Routes>
             </div>
         </div>
         </Provider>
